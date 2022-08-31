7cf11ac52d0d095b6a573d8bdd6001ef92ea365c
Yarden Ben Shimon <yarden.benshimon@operative.com>
2022-08-31 11:35:51 -0400

    test deploy

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          hello world1!
+          hello world2!
         </p>
         <a
           className="App-link"
